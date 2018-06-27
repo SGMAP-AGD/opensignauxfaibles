253d59ee5d5023a10407e8d8a6c4d7149313c027
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2018-06-27 11:29:07 +0200

    More data !

--- dbmongo/js/algo2/EtablissementFinalize.js
+++ dbmongo/js/algo2/EtablissementFinalize.js
@@ -132,24 +132,24 @@ function finalize(k, v) {
         })
 
         // delais
-    // Object.keys(v.delai).map(
-    //     function (hash) {
-    //         var delai = v.delai[hash]
-    //         var date_creation = new Date(Date.UTC(delai.date_creation.getUTCFullYear(), delai.date_creation.getUTCMonth(), 1, 0, 0, 0, 0))
-    //         var date_echeance = new Date(Date.UTC(delai.date_echeance.getUTCFullYear(), delai.date_echeance.getUTCMonth(), 1, 0, 0, 0, 0))
-    //         var pastYearTimes = generatePeriodSerie(date_creation, date_echeance).map(function (date) { return date.getTime() })
-    //         pastYearTimes.map(
-    //             function(time){
-    //                 if (time in value) {
-    //                     value[time].delai = true;
-    //                     value[time].duree_delai = delai.duree_delai
-    //                     value[time].montant_echeancier = delai.montant_echeancier
+    Object.keys(v.delai).map(
+        function (hash) {
+            var delai = v.delai[hash]
+            var date_creation = new Date(Date.UTC(delai.date_creation.getUTCFullYear(), delai.date_creation.getUTCMonth(), 1, 0, 0, 0, 0))
+            var date_echeance = new Date(Date.UTC(delai.date_echeance.getUTCFullYear(), delai.date_echeance.getUTCMonth(), 1, 0, 0, 0, 0))
+            var pastYearTimes = generatePeriodSerie(date_creation, date_echeance).map(function (date) { return date.getTime() })
+            pastYearTimes.map(
+                function(time){
+                    if (time in value) {
+                        value[time].delai = true;
+                        value[time].duree_delai = delai.duree_delai
+                        value[time].montant_echeancier = delai.montant_echeancier
 
-    //                 }
-    //             }
-    //         )
-    //     }
-    // )
+                    }
+                }
+            )
+        }
+    )
 
     // defaillance - On prend la date de l'évènement le plus proche dans l'avenir par rapport à period
     Object.keys(v.altares).map(
